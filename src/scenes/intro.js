06b1b5108bb2d0564b46ab850e0cbae3137ff02f
luq-mne <luqman.yazrie@outlook.com>
2024-12-27 09:05:49 +0800

    start things

--- src/scenes/intro.js
+++ src/scenes/intro.js
@@ -2,7 +2,7 @@ export default function Intro(k) {
     k.scene("intro", () => {
         const title = k.add([
             k.text("Boeing 737NG Flight Management System Simulator", {
-                font: "Arial",
+                font: "arial",
                 size: 50,
                 width: 500
             }),
@@ -11,8 +11,8 @@ export default function Intro(k) {
         ])
 
         const descriptionn = k.add([
-            k.text("A simulator for the Boeing 737NG Flight Management System. This is a project for the University of Southampton, and is not affiliated with Boeing in any way.", {
-                font: "Arial",
+            k.text("A web-based simulator of the Boeing 737-800's flight management system (FMS). Not affliated with Boeing, this is a project for educational purposes only.", {
+                font: "arial",
                 size: 20,
                 width: 500
             }),
@@ -24,13 +24,13 @@ export default function Intro(k) {
         const noticeBox = k.add([
             k.rect(500, 150),
             k.pos(k.width() / 2, 400),
-            k.color(255, 255, 255),
+            k.color(241,196,15),
             k.anchor("top")
         ]);
 
         const noticeText = k.add([
             k.text("This project is optimised only for 16:9 (laptop/desktop) displays and is not designed with mobile friendliness in mind.", {
-                font: "Arial",
+                font: "arial",
                 size: 30,
                 width: 480
             }),
@@ -39,5 +39,23 @@ export default function Intro(k) {
             k.anchor("top")
         ])
 
+        const startButton = k.add([
+            k.rect(500, 50),
+            k.pos(k.width() / 2, 560),
+            k.color(255, 255, 255),
+            k.anchor("top")
+        ]);
+
+        const startText = k.add([
+            k.text("Start", {
+                font: "arial",
+                size: 30,
+                width: 500,
+                align: "center"
+            }),
+            k.pos(k.width() / 2, 575),
+            k.color(0, 0, 0),
+            k.anchor("top")
+        ]);
     });
 }
