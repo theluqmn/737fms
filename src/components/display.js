5ab35ce87427833df5624f8a7a5a0f07a6723950
luq-mne <luqman.yazrie@outlook.com>
2024-12-27 22:53:01 +0800

    progress - rounded stuff, trying out some lines

--- src/components/display.js
+++ src/components/display.js
@@ -1,13 +1,17 @@
 export function displayComponent(k) {
     const frame = k.add([
-        k.rect(400, 340),
+        k.rect(400, 340, {
+            radius: 20
+        }),
         k.pos(300, 130),
         k.color(25,25,25),
         k.anchor("top")
     ]);
 
     const screen = k.add([
-        k.rect(360, 300),
+        k.rect(360, 300, {
+            radius: 15
+        }),
         k.pos(300, 150),
         k.color(0,0,0),
         k.anchor("top")
@@ -17,27 +21,42 @@ export function displayComponent(k) {
     const l1 = displayButton(k, 80, 200, () => {
         console.log("left 1");
     });
+    const ml1 = displayButtonLine(k, 110, 200);
     const d1 = displayTextPrimary(k, 125, 160, "PERF", "right");
 
     const l2 = displayButton(k, 80, 240);
-    const d2 = displayTextSecondary(k, 125, 180, "  GW/CRZ CG", "left");
-    const d3 = displayTextPrimary(k, 125, 200, "158.9/19.1%");
-    displayButtonLine(k, 110, 200);
+    const ml2 = displayButtonLine(k, 110, 240);
+
     const l3 = displayButton(k, 80, 280);
-    displayButtonLine(k, 110, 240);
+    const ml3 = displayButtonLine(k, 110, 280);
+
     const l4 = displayButton(k, 80, 320);
-    displayButtonLine(k, 110, 280);
+    const ml4 = displayButtonLine(k, 110, 320);
+
     const l5 = displayButton(k, 80, 360);
+    const ml5 = displayButtonLine(k, 110, 360);
+
     const l6 = displayButton(k, 80, 400);
-    const d4 = displayTextPrimary(k, 125, 400, "< INDEX");
+    const ml6 = displayButtonLine(k, 110, 400);
 
     // right side
     const r1 = displayButton(k, 520, 210);
+    const mr1 = displayButtonLine(k, 490, 210);
+
     const r2 = displayButton(k, 520, 250);
+    const mr2 = displayButtonLine(k, 490, 250);
+
     const r3 = displayButton(k, 520, 290);
+    const mr3 = displayButtonLine(k, 490, 290);
+
     const r4 = displayButton(k, 520, 330);
+    const mr4 = displayButtonLine(k, 490, 330);
+
     const r5 = displayButton(k, 520, 370);
+    const mr5 = displayButtonLine(k, 490, 370);
+    
     const r6 = displayButton(k, 520, 410);
+    const mr6 = displayButtonLine(k, 490, 410);
 }
 
 function displayButton(k, x, y, onClick) {
