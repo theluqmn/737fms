7641e61213f98667f54a4175de9c37208efeb905
luq-mne <luqman.yazrie@outlook.com>
2024-12-27 16:07:55 +0800

    moving the functions away

---
+++ src/components/display.js
@@ -0,0 +1,65 @@
+export function displayComponent(k) {
+    const frame = k.add([
+        k.rect(400, 340),
+        k.pos(300, 130),
+        k.color(25,25,25),
+        k.anchor("top")
+    ]);
+
+    const screen = k.add([
+        k.rect(360, 300),
+        k.pos(300, 150),
+        k.color(0,0,0),
+        k.anchor("top")
+    ]);
+
+    // left side
+    const l1 = displayButton(k, 80, 215, () => {
+        console.log("left 1");
+    });
+    const l2 = displayButton(k, 80, 255);
+    const l3 = displayButton(k, 80, 295);
+    const l4 = displayButton(k, 80, 335);
+    const l5 = displayButton(k, 80, 375);
+    const l6 = displayButton(k, 80, 415);
+
+    // right side
+    const r1 = displayButton(k, 520, 215);
+    const r2 = displayButton(k, 520, 255);
+    const r3 = displayButton(k, 520, 295);
+    const r4 = displayButton(k, 520, 335);
+    const r5 = displayButton(k, 520, 375);
+    const r6 = displayButton(k, 520, 415);
+}
+
+function displayButton(k, x, y, onClick) {
+    const button = k.add([
+        k.rect(28, 20),
+        k.pos(x, y),
+        k.color(10,10,10),
+        k.anchor("top"),
+        k.area(),
+        k.scale(1),
+        "button"
+    ]);
+
+    const highlight = k.add([
+        k.rect(22, 2),
+        k.pos(x, y + 10),
+        k.color(225,225,225),
+        k.anchor("center"),
+        k.scale(1),
+    ]);
+
+    button.onClick(() => {
+        button.scaleTo(0.95);
+        highlight.scaleTo(0.95);
+        setTimeout(() => {
+            button.scaleTo(1);
+            highlight.scaleTo(1);
+            if (onClick) onClick(); // Run the callback function if it's provided
+        }, 100);
+    });
+
+    return button;
+}
