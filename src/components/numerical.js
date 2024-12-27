74d08b9edeea35577363917290fff34cadf507f5
luq-mne <luqman.yazrie@outlook.com>
2024-12-28 07:41:06 +0800

    stunning

--- src/components/numerical.js
+++ src/components/numerical.js
@@ -1,15 +1,32 @@
 export default function Numerical(k) {
-
+    button(k, 115, 676, "1");
+    button(k, 160, 676, "2");
+    button(k, 205, 676, "3");
+    // row 2
+    button(k, 115, 724, "4");
+    button(k, 160, 724, "5");
+    button(k, 205, 724, "6");
+    // row 3
+    button(k, 115, 772, "7");
+    button(k, 160, 772, "8");
+    button(k, 205, 772, "9");
+    // row 4
+    button(k, 115, 820, ".");
+    button(k, 160, 820, "0");
+    button(k, 205, 820, "+/=");
 }
 
 function button(k, x, y, textInput, onClick) {
-    let size = 22;
-    let adjust = 10;
+    let size = 24;
+    let adjust = 8;
+
+    if (textInput.length > 1) {
+        size = 14;
+        adjust = 12;
+    }
 
     const button = k.add([
-        k.rect(38, 38, {
-            radius: 3
-        }),
+        k.circle(18),
         k.pos(x, y),
         k.color(10,10,10),
         k.anchor("top"),
