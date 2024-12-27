5fc2abe3a6f521310666ba396b09228df7fe0b99
luq-mne <luqman.yazrie@outlook.com>
2024-12-27 11:17:14 +0800

    started work on FMS frame

--- src/scenes/fms.js
+++ src/scenes/fms.js
@@ -5,5 +5,16 @@ export default function FMS(k) {
             k.pos(10,10),
             k.color(255,255,255)
         ]);
+
+        frameComponent(k);
     });
 };
+
+function frameComponent(k) {
+    const frameFMS = k.add([
+        k.rect(500, 800),
+        k.pos(k.width() / 2, k.height() / 2),
+        k.color(20,20,20),
+        k.anchor("center")
+    ])
+}
