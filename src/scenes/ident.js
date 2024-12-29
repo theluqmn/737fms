bb3575ee986c8e1a9d094657c5114e719552c448
luq-mne <luqman.yazrie@outlook.com>
2024-12-29 08:21:43 +0800

    basic guide on ident frfr

--- src/scenes/ident.js
+++ src/scenes/ident.js
@@ -16,6 +16,29 @@ IDENT
 
 Shows details about the aircraft. This is the default page
 that is shown after the FMS is initialized.
+
+-
+BASIC GUIDE
+
+The display has 12 lines of text - 6 headings and 6 texts.
+Heading lines are smaller and are located at the top of
+each text. Texts sit right next to its corresponding
+Line Select Key (LSK).
+
+On lines with an arrow (</>), pressing its LSK will
+change the page to the page specified by the line. For
+quick access, you can use the 15 mode keys which is located
+below the display.
+
+The bottom part the display is reserved for the scratchpad.
+Try typing using the keyboard on the FMS - the text will be
+stored in the scratchpad. In pages where you are required to
+enter data, simply type the data in the scratchpad and press
+the corresponding LKS to enter the data. On lines with LSKs
+that does not have a function, or lines that is output-only,
+the text can be copied to the scratchpad by pressing its LSK.
+Use the SP key to add spaces, DEL key to delete characters,
+and CLR to clear the scratchpad.
                 `, {
                     font: "Arial",
                     size: 18,
